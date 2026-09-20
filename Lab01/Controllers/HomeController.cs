24e8b4e82bd27023716232e1b971f32425cce3c8
kimngan <kimngan@DANGHUNG>
2026-09-21 00:22:11 +0700

    Add project files.

---
+++ Lab01/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Lab01a()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult BT1_Lab01b()
+        {
+            return View();
+        }
+        public ActionResult BT2_Lab01b()
+        {
+            return View();
+        }
+
+        public ActionResult BT3_Lab01b()
+        {
+            return View();
+        }
+
+        public ActionResult BT4_Lab01b()
+        {
+            return View();
+        }
+
+        public ActionResult BT5_Lab01b()
+        {
+            return View();
+        }
+
+        public ActionResult BT1_Lab01b2()
+        {
+            return View();
+        }
+
+        public ActionResult BT2_Lab01b2()
+        {
+            return View();
+        }
+        public ActionResult BT3_Lab01b2()
+        {
+            return View();
+        }
+
+        public ActionResult BT4_Lab01b2()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab01/Controllers/TH3Controller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01.Controllers
+{
+    public class TH3Controller : Controller
+    {
+        // GET: TH3
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+
+
+        public ActionResult repass()
+        {
+            return View();
+        }
+
+        public ActionResult regist()
+        {
+            return View();
+        }
+    }
+}
